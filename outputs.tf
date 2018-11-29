42fa54a9ad2a458c779f1b7c04fb35643cee5a4a
EC2 Default User <ec2-user@ip-172-31-12-191.us-east-2.compute.internal>
2018-11-29 11:41:58 +0000

    added outputs to root module

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,31 @@
-# ------------------- storage/outputs.tf-------------------
+#--------------------root/outputs.tf-----------------------
+
+# ------------------- storage outputs-------------------
 
 output "Bucket Name" {
   value = "${module.storage.bucketname}"
+}
+
+# ------------------- networking outputs-------------------
+
+output "Public Subnets" {
+  value = "${join(", ", module.networking.public_subnets")}"
+}
+
+output "Public IPs" {
+  value = "${join(", ", module.networking.subnet_ips")}"
+}
+
+output "Public Security Groups" {
+  value = "${module.networking.public_sg)}"
+}
+
+# ------------------- compute outputs-------------------
+
+output "Public Instance IDs" {
+  value = "${module.compute.server_id}"
+}
+
+output "Public Instance IPs" {
+  value = "${module.compute.server_ip}"
 }
