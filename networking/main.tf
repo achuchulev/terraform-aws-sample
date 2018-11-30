f695820a9a91fd75ff0b94b4febf557b22fba67d
achuchulev <42144722+achuchulev@users.noreply.github.com>
2018-11-30 13:14:05 +0200

    Update main.tf

--- networking/main.tf
+++ networking/main.tf
@@ -25,7 +25,7 @@ resource "aws_route_table" "tf_public_rt" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.tf_internet_gateway_id}"
+    gateway_id = "${aws_internet_gateway.tf_internet_gateway.id}"
   }
 
   tags {
